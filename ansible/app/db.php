e2d523f608015f58efc9114c536fb0af37ee2bb3
Ubuntu <ubuntu@ip-172-31-0-233.ap-south-1.compute.internal>
2026-08-18 19:16:15 +0000

    Restore secure database connection files

---
+++ ansible/app/db.php
@@ -0,0 +1,12 @@
+<?php
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
+$database = getenv("DB_NAME");
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+?>
